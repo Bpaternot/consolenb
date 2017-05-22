04bfe21d1f39de1ab74779c6deacd2609a4db5bc
Valentin Selve <val.selve@gmail.com>
2017-05-22 19:04:26 +0200

    set up of controllers done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,27 @@
 class BookingsController < ApplicationController
+
+  def new
+    # create empty coquille
+    # find console
+  end
+
+  def create
+    # create coquille according to booking_params and current_user
+    # find console
+    # link booking to console
+    # save new console
+  end
+
+  def edit
+    # find console
+    # find current booking
+  end
+
+  def update
+    # find console
+    # find current booking
+    # link updated booking to console
+    # save updated booking
+    # redirect to dashboard
+  end
 end
--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -1,2 +1,48 @@
 class ConsolesController < ApplicationController
+  # before action to write for show, edit, update, delete
+
+  def index
+    # find all consoles
+  end
+
+  def show
+    # find one console
+    # show this console
+  end
+
+  def new
+    # create empty coquille
+  end
+
+  def create
+    # create coquille with console_params
+    # save the new console
+    # redirect on dashboard
+  end
+
+  def edit
+    # find one console
+  end
+
+  def update
+    # find one console
+    # update datas
+    # redirect to dashboard
+  end
+
+  def delete
+    # find one console
+    # delete the console
+    # redirect to dashboard
+  end
+
+  private
+
+  def set_console
+    # find the current console
+  end
+
+  def console_params
+    # permitted params
+  end
 end
