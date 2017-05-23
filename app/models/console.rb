0d1289bc93fa438c1be0414dd4dccadce9ae5089
arnaud boscq <arnaud@itsmycar.fr>
2017-05-23 11:55:30 +0200

    dashboard v1

--- app/models/console.rb
+++ app/models/console.rb
@@ -2,6 +2,6 @@ class Console < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :type, in [PS4, PS3, PS2, PSOne, XBOX, XBOX One, Nintendo Ness, Wii, SEGA, Nintendo 64, Gamecube]
+
 
 end
