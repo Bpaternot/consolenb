bbccc306a313ad59a25cdf1893646129ffd0672c
Cyrielle Boclet <cyrielle.boclet@gmail.com>
2017-05-24 05:19:21 -0400

    Dashboard - listing bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,8 @@
 class Booking < ApplicationRecord
+  STATUS = ["approved", "pending", "declined", "past"]
+
   belongs_to :user
   belongs_to :console
   validates :duration, presence: true
+  validates :status, inclusion: { in: STATUS }
 end
