6ac7b55e088e02cf58664145ccad95cc1823b0c6
arnaud boscq <arnaud@itsmycar.fr>
2017-05-25 11:50:31 +0200

    review controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+   def new
+    # we need @restaurant in our `simple_form_for`
+    @console = Console.find(params[:console_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.console = Console.find(params[:console_id])
+    if @review.save
+      redirect_to dashboard_path
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
