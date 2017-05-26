3752556622f90019ee4e090a112183c23d2086f5
Valentin Selve <val.selve@gmail.com>
2017-05-26 13:18:18 +0200

    index updated

--- app/assets/javascripts/init_datepicker.js
+++ app/assets/javascripts/init_datepicker.js
@@ -1,6 +1,20 @@
-$('.datepicker').datepicker({
-  format: "dd/mm/yyyy",
-  weekStart: 1,
-  autoclose: true,
-  startDate: new Date()
-});
+$(function() {
+        $(".datepicker").datepicker({
+          format: "dd/mm/yyyy",
+          weekStart: 1,
+          startDate: new Date()
+        });
+    });
+
+
+// $('.datepicker').datepicker({
+//   format: "dd/mm/yyyy",
+//   weekStart: 1,
+//   autoclose: true,
+//   startDate: new Date()
+// });
+// $('#datepicker').on('changeDate', function() {
+//     $('#my_hidden_input').val(
+//         $('#datepicker').datepicker('getFormattedDate')
+//     );
+// });
